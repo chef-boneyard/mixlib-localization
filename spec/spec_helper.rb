aabaaa6c0bbbca8bfcbc77120f421108383efbc8
Chris Walters <cw@opscode.com>
2010-10-08 11:18:11 -0700

    Fixing spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,6 @@ require 'pp'
 require 'rubygems'
 $:.unshift File.expand_path(File.dirname(__FILE__) + "/../lib/")
 require 'mixlib/localization'
+require 'mixlib/localization/messages'
 
 include Mixlib::Localization
