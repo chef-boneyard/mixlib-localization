05119f9607306f63929e3540d2ba88c7b27e1e02
Nuo Yan <nuo@opscode.com>
2011-02-25 15:46:41 -0800

    remove useless messages

--- lib/mixlib/localization/messages.rb
+++ lib/mixlib/localization/messages.rb
@@ -2023,27 +2023,6 @@ module Mixlib
             "zh_cn" => "加载环境时出现系统内部问题，请稍候重试。"
           }
         },
-        "opscode-chef-webui-environments-cookbooks-403" => {
-          "message_id" => "33020",
-          "languages" => {
-            "en_us" => "Permission Denied. You do not have permission on this environment to list cookbooks.",
-            "zh_cn" => "对不起，您无权在此环境中查看食谱列表。"
-          }
-        },
-        "opscode-chef-webui-environments-cookbooks-show-403" => {
-          "message_id" => "33021",
-          "languages" => {
-            "en_us" => "Permission Denied. You do not have permission on this environment to view the cookbook.",
-            "zh_cn" => "对不起，您无权在此环境中查看此食谱。"
-          }
-        },
-        "opscode-chef-webui-environments-cookbooks-show-404" => {
-          "message_id" => "33022",
-          "languages" => {
-            "en_us" => "Selected cookbook or environment not found.",
-            "zh_cn" => "无法找到此食谱或所在的环境。"
-          }
-        },
         "opscode-chef-webui-environments-default-edit-405" => {
           "message_id" => "33023",
           "languages" => {
